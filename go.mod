4cace9fceed2a79ff87b908cbc89b9529b7b1ce6
Andrii Butko <andrii.butko@eleks.com>
2020-08-08 06:22:28 +0300

    Add bot sample code

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/andreybutko/stocks-telegram-bot
 
-go 1.13
+go 1.14
---
+++ main.go
@@ -0,0 +1,39 @@
+﻿package main
+
+import (
+	"log"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+func main() {
+	println("Ciao!")
+
+	bot, err := tgbotapi.NewBotAPI("")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil { // ignore any non-Message Updates
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		bot.Send(msg)
+	}
+
+}
